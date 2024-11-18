cf439b108c677346c683125355071eabf46c3e37
user <yws7075@naver.com>
2024-11-18 18:58:07 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:eos_todolist/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EOS ToDoList',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        fontFamily: 'Pretendard',
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
